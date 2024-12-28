cfc873c1b8ab208ece8c38428daf596873177df1
alaminsframe <helloalaminsheikh@gmail.com>
2024-12-29 00:47:21 +0600

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-red-700">hello </h1>
+    </>
+  )
+}
+
+export default App
